29f6d0063e66fd9bdb7113edd1bf963f232e6ee3
AlirezaBayat <alireza@lixi.com.au>
2021-02-09 19:06:42 +1100

    Updates

---
+++ precache-manifest.bbcd86b6fb695209b683c7ad021497b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93800b25522d5f24fbbb7ed79f9ada66",
+    "url": "/Turbo_Demo/index.html"
+  },
+  {
+    "revision": "131f2db83fea26b39241",
+    "url": "/Turbo_Demo/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "961d55c9b25eb86aa400",
+    "url": "/Turbo_Demo/static/js/2.b4f5a5fd.chunk.js"
+  },
+  {
+    "revision": "416c6512f412a0f0d0c155e219e18612",
+    "url": "/Turbo_Demo/static/js/2.b4f5a5fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "131f2db83fea26b39241",
+    "url": "/Turbo_Demo/static/js/main.141a81fc.chunk.js"
+  },
+  {
+    "revision": "a9527d67ba1e29dac608",
+    "url": "/Turbo_Demo/static/js/runtime-main.f753ad8f.js"
+  }
+]);
